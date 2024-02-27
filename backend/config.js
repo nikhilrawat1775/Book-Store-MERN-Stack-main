fc0643e731eb4ae17c514eeece8c393934ae939e
nikhil <nikhilrawat3171@gmail.com>
2024-02-27 11:57:23 +0530

    commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://nikhil1775be21:tI0Es5dcAWX7AoTu@cluster0.cmcghoa.mongodb.net/?retryWrites=true&w=majority"
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
